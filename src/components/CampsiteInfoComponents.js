8814d971591d2ed93ec5bb4c5537add0e5ee497f
Manoj Pun <manoj.haki@gmail.com>
2021-06-19 15:00:23 -0400

    finish assign

--- src/components/CampsiteInfoComponents.js
+++ src/components/CampsiteInfoComponents.js
@@ -2,23 +2,11 @@ import React, { Component } from 'react'
 import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
 
 export default class CampsiteInfoComponents extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-           selectedCampsite: null
-        };
-
-    }
-
-    onCampsiteSelect(campsite){
-
-        this.setState({selectedCampsite: campsite});
-    }
     renderCampsite(campsite) {  
   
         return(
         <div className="col-md-5 m-1">
-            <Card onClick={()=> this.onCampsiteSelect(campsite)}>
+            <Card>
                     <CardImg width="100%" src={campsite.image} alt={campsite.name} />
                    <CardImgOverlay>
                        <CardTitle>{campsite.name}</CardTitle>
@@ -28,23 +16,32 @@ export default class CampsiteInfoComponents extends Component {
         )
     }
 
-    renderComments(comments) {
-        if (comments) {
+    renderComments(comments){
+        if(comments){
+
             return(
                 <div className="col-md-5 m-1">
                     <h4>Comments</h4>
-                    {comments.map(current=> <div > {current.text}  <br></br> --{current.author} ,{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(current.date)))}</div>
-                    )}
-                   
-                </div>
-            
-            )
-            return <div/>
-        }
-      
-    
-    }
+                    {
+                        comments.map((comment)=>(
+                            <div>
+                                {comment.text}<br></br> --{comment.author} ,{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                            </div>
 
+                        ))
+
+                    }
+
+                </div>
+
+
+            )
+        }
+        return <div/>
+       
+
+    }
+        
     render() {
         if (this.props.campsite) {
             return (
